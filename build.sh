91416fdbf4b1379f36a7ec06d240fcbdef3611c4
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2019-08-23 20:14:01 +0100

    Prepare repo for go

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 set +x
+set +e
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export GOPATH="$CURRENT_DIR"
+
+mkdir -p bin/
+export GOBIN="$CURRENT_DIR/bin"
+
+go install src/main.go
