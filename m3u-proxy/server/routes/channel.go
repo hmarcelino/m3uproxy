6b6d42057e74f3f8c63e8ce80aef4bcf91cdb9f0
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2019-08-27 22:00:58 +0100

    Some improvements in the code that load the stream

--- m3u-proxy/server/routes/channel.go
+++ m3u-proxy/server/routes/channel.go
@@ -12,8 +12,9 @@ import (
 	"net/url"
 )
 
-const QUERY_PARAM_LOCATION = "location"
-const CHANNEL_ID_HEADER = "X-ChannelId"
+const QueryParamLocation = "location"
+const HeaderChannelId = "X-ChannelId"
+const HeaderRange = "Range"
 
 func ChannelRoute(config *config.Config) (string, func(w http.ResponseWriter, r *http.Request)) {
 	return "/channels/{id}", func(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +22,11 @@ func ChannelRoute(config *config.Config) (string, func(w http.ResponseWriter, r 
 		channelId := vars["id"]
 
 		channelAddr, err := db.LookupChannel(channelId)
+
+		query := r.URL.Query()
+		newChannelAddr := query.Get(QueryParamLocation)
+		rangeValue := r.Header.Get(HeaderRange)
+
 		if err != nil {
 			webutils.NotFound(w)
 			return
@@ -29,46 +35,56 @@ func ChannelRoute(config *config.Config) (string, func(w http.ResponseWriter, r 
 		dump, err := httputil.DumpRequest(r, false)
 		log.Printf("%q\r\nRemoteAddr: %s", dump, r.RemoteAddr)
 
-		// Channel location override
-		location := r.URL.Query().Get(QUERY_PARAM_LOCATION)
-		if location != "" {
-			newUrl, err := url.Parse(location)
+		if newChannelAddr != "" {
+			newUrl, err := url.Parse(newChannelAddr)
 			if err != nil {
-				webutils.BadGateway("Invalid location address", err, w)
+				webutils.BadRequest("Invalid channel address override: "+newChannelAddr, err, w)
 				return
 			}
 
-			log.Printf("Got new location for channel: %s, location: %s\n", channelId, location)
-			newLocationChannelAddr := &db.Channel{Id: channelId, Source: newUrl}
-			newProxy(config, newLocationChannelAddr).ServeHTTP(w, &http.Request{URL: newLocationChannelAddr.Source})
-			return
+			channelAddr = &db.Channel{Id: channelId, Source: newUrl}
 		}
 
-		// fallBack to channel address
 		request := http.Request{URL: channelAddr.Source}
 		request.Header = map[string][]string{}
-		request.Header.Add(CHANNEL_ID_HEADER, channelId)
-		newProxy(config, channelAddr).ServeHTTP(w, &request)
+		request.Header.Add(HeaderChannelId, channelId)
+
+		if rangeValue != "" {
+			request.Header.Add(HeaderRange, rangeValue)
+		}
+
+		proxy := newProxy(channelAddr)
+		if newChannelAddr == "" {
+			proxy.ModifyResponse = GetResponseModifier(config)
+		}
+
+		log.Printf("Proxying request for channel %s %s redirect=%t",
+			channelId,
+			channelAddr.Source.String(),
+			newChannelAddr != "")
+
+		proxy.ServeHTTP(w, &request)
 	}
 }
 
-func newProxy(config *config.Config, channel *db.Channel) *httputil.ReverseProxy {
+func newProxy(channel *db.Channel) *httputil.ReverseProxy {
 	addr := channel.Source
 	uHost, _ := url.Parse(addr.Scheme + "://" + addr.Host)
+	return httputil.NewSingleHostReverseProxy(uHost)
+}
 
-	proxy := httputil.NewSingleHostReverseProxy(uHost)
-	proxy.ModifyResponse = func(resp *http.Response) error {
-
+func GetResponseModifier(config *config.Config) func(resp *http.Response) error {
+	return func(resp *http.Response) error {
 		isRedirect := resp.StatusCode == http.StatusFound ||
 			resp.StatusCode == http.StatusSeeOther ||
 			resp.StatusCode == http.StatusTemporaryRedirect
 
-		channelId := resp.Request.Header.Get(CHANNEL_ID_HEADER)
+		channelId := resp.Request.Header.Get(HeaderChannelId)
 
 		if isRedirect && channelId != "" {
-			newReq, _ := url.Parse(NewChannelProxy(config, channelId))
+			newReq, _ := url.Parse(GetChanneUrl(config, channelId))
 			query := newReq.Query()
-			query.Set(QUERY_PARAM_LOCATION, resp.Header.Get("Location"))
+			query.Set(QueryParamLocation, resp.Header.Get("Location"))
 
 			newReq.RawQuery = query.Encode()
 			resp.Header.Set("Location", newReq.String())
@@ -76,13 +92,11 @@ func newProxy(config *config.Config, channel *db.Channel) *httputil.ReverseProxy
 
 		return nil
 	}
-
-	return proxy
 }
 
 // The return should match the previous route pattern.
 // Http://host:port/channels/channelId
-func NewChannelProxy(config *config.Config, id string) string {
+func GetChanneUrl(config *config.Config, id string) string {
 	return fmt.Sprintf(
 		"http://%s:%d/channels/%s",
 		config.Server.Hostname,
